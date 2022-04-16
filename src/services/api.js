ff96273ae0c4c52c6947a8a6497d70f0cc1e3de8
andrpoch <andrpoch25@gmail.com>
2022-04-16 23:59:14 +0300

    add api service

--- src/services/api.js
+++ src/services/api.js
@@ -1,34 +1,9 @@
 const BASE_URL = 'https://pixabay.com/api/?';
 const API_KEY = '25752976-c432b2b3d55e7cabed38c70ac';
 
-export default class api { 
-   constructor () {
-      this.page = 1;
-      this.searchQuery ='';
-   }
-   getImages() {
-      const fetchUrl = `${BASE_URL}?key=${API_KEY}q=${this.searchQuery}&page=${this.page}&image_type=photo&orientation=horizontal&per_page=12`;
-      return fetch(fetchUrl).then((response) => {
-         if (response.ok) {
-            return response.json();
-         }
-         return Promise.reject(new Error(`No images ${this.searchQuery}`));
-      }).then((responce) => {
-         this.incrementPage();
-         return responce;
-      });
-   }
-   incrementPage() {
-      this.page += 1;
-   };
-   resetPage() {
-      this.page=1;
-   }
-   get query (){
-      return this.searchQuery;
-   }
-   set query(newQuery) {
-      this.searchQuery = newQuery;
-   };
+cosnt fetchImages = (searchQuery,page)=>{
+   const fetchUrl = `${BASE_URL}?key=${API_KEY}q=${searchQuery}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`;
+ return fetch(fetchUrl).then((res) => res.json());
 };
 
+export default fetchImages;
